e55933b4344ff11c7fc8f5e4f8cfb01fb1668344
Peter Ha <34954149+petehaha@users.noreply.github.com>
2021-04-07 23:17:20 -0500

    Completed 2.1 Code

--- Deliverables/2/2.1/back-end.py
+++ Deliverables/2/2.1/back-end.py
@@ -1,1 +1,18 @@
 import json
+import fileinput
+
+inputObjects = []
+d = json.JSONDecoder()
+e = json.JSONEncoder()
+
+for line in fileinput.input():
+    if line.isspace():
+        continue
+    line = line.replace("\'","\"")
+    object = d.decode(line)
+    inputObjects.append(object)
+
+inputObjects.sort(key=lambda item: list(item.values()))
+sortedArray = inputObjects
+
+print(e.encode(sortedArray))
