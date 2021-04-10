c4aa0e2d3ebaea07c5d0cc5375d2f330c2ffa55b
ReyTangerine <rt9088@hotmail.com>
2021-04-09 23:26:40 -0500

    Yikes this should have 34 tests fail? Hopefully?

--- Deliverables/2/2.2/frontend.py
+++ Deliverables/2/2.2/frontend.py
@@ -1,7 +1,7 @@
 import json
 import sys
 import math
-#sys.path.append('../2.1/')
+sys.path.append('../2.1/')
 import backend
 
 intermediateObject = ""
@@ -9,6 +9,20 @@ d = json.JSONDecoder()
 e = json.JSONEncoder()
 
 str1 = sys.stdin.read()
+
+def validObject(content):
+    contentList = list(content)
+    if "{" in content and "}" in content and contentList.count("}") == contentList.count("{"):
+        return True
+    else:
+        return False
+
+## Used with JSON to clean strings
+## Removes the start of the string until the '{' is reached
+def string_cleaner(string):
+    while string[0] != '{':
+        string = string[1:]
+    return(string)
 
 def parseJSON(input):
     outputObjects = []
@@ -20,22 +34,50 @@ def parseJSON(input):
     # Since each dict ends with "}", split on it and remove the last trailing \n.
     # Then add the "}" back in.
     parsedJSON = json.loads(json.dumps(input)).split("}")
+    print(str(parsedJSON))
+
+    # Removing Empty elements in the dictionary
     parsedJSON.pop()
+    for parsedTokenNumber in range(len(parsedJSON) - 1):
+        if parsedJSON[parsedTokenNumber] == '':
+            parsedJSON.pop(parsedTokenNumber)
+
+    # Since some jerks added in characters for each line between the dictionary entries
+    # We need to remove those characters (now in the start of the parsedJSON list entries)
+    # We can remove these bad boys by eliminating tokens that aren't '['
+    for parsedTokenNumber in range(len(parsedJSON)- 1):
+        if parsedJSON[parsedTokenNumber] != '{':
+            newString = string_cleaner(parsedJSON[parsedTokenNumber])
+            parsedJSON[parsedTokenNumber] = newString
+    #print("This is parsedJSON: " + str(parsedJSON))
 
     for JSONobject in parsedJSON:
         JSONobject = JSONobject + "}"
-        object = d.decode(JSONobject)
+        if validObject(JSONobject):
+            object = d.decode(JSONobject)
 
-        # Cleaning up the objects from incorrect values
-        # Ensuring that all values are between 0 & 24
-
-        if object['content'] >= 0 and object['content'] <= 24:
-            outputObjects.append(object)
+            # Cleaning up the objects from incorrect values
+            # Ensuring that all values are between 0 & 24
+            checking = object.get("content")
+            if checking != None:
+                contentValue = object['content']
+                object.clear()
+                object['content'] = contentValue
+                if type(object['content']) is int:
+                    if object['content'] >= 0 and object['content'] <= 24:
+                        outputObjects.append(object)
+                    else:
+                        pass
+                else:
+                    pass
+            else:
+                pass
         else:
             pass
     return outputObjects
 
 def FrontendProcessing(FrontendInput):
+    #print("This is Front End Input: " + str(FrontendInput))
     output = []
     mainOutput = []
     output.extend(parseJSON(FrontendInput))
