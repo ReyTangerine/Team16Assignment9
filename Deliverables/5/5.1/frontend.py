a41511ff20fc3c0489b04f54356dadb51ade2f1d
ReyTangerine <rt9088@hotmail.com>
2021-04-26 10:01:59 -0500

    Checking if the first two tests are valid!

---
+++ Deliverables/5/5.1/frontend.py
@@ -0,0 +1,24 @@
+import sys
+import json
+
+print("Hello World!")
+
+"""
+from Backgammon_Class import Proxy_Backgammon_Board
+
+
+def inputdecoder(board, color, dice, move):
+    return [board, [color, dice, move]]
+
+
+d = json.JSONDecoder()
+e = json.JSONEncoder()
+
+str = sys.stdin.read()
+input = d.decode(str)
+
+board = Proxy_Backgammon_Board(*inputdecoder(*input))
+
+print(e.encode(board.getSolution()))
+
+"""
