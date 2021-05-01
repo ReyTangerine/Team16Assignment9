39f3c9344fc42c496d2e47c08163fff32aeadf2d
Peter Ha <34954149+petehaha@users.noreply.github.com>
2021-04-30 23:39:20 -0500

    Experimenting with false returns

--- Deliverables/5/5.1/frontend.py
+++ Deliverables/5/5.1/frontend.py
@@ -17,6 +17,11 @@ dice = input[2]
 game.set_board(board)
 if color == "black":
     game.turnNum = 1
-print(e.encode(game.turn(dice)))
+
+retVal = game.turn(dice)
+if retVal is False:
+    print(False)
+else:
+    print(e.encode(retVal))
 
 
