8e38eead62d42d31c3bc23c8178d5765b33f9a9b
Peter Ha <34954149+petehaha@users.noreply.github.com>
2021-04-30 23:21:19 -0500

    Fixed random_move() bug in 5.1

---
+++ Deliverables/5/5.2/frontend.py
@@ -0,0 +1,22 @@
+from GameTools import Player, Game
+import sys
+import json
+
+d = json.JSONDecoder()
+e = json.JSONEncoder()
+
+str = sys.stdin.read()
+input = d.decode(str)
+
+game = Game("player1", "player2")
+
+board = input[0]
+color = input[1]
+dice = input[2]
+
+game.set_board(board)
+if color == "black":
+    game.turnNum = 1
+print(e.encode(game.turn(dice)))
+
+
